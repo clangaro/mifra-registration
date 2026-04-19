93e01696fda960a3078e431a63b167ac18beac7b
clangaro <carolinalangaro@berkeley.edu>
2026-04-19 13:27:30 -0700

    adding more smoothness

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ DOWNSCALE_FACTOR = 0.5
 
 # Temporal smoothing to reduce trembling.
 # Higher window = smoother but slower response to real rotation changes.
-SMOOTHING_WINDOW = 11
+SMOOTHING_WINDOW = 15
 
 # Circular crop to remove border artefacts from rotation.
 APPLY_CIRCULAR_CROP = True
